675c359ed4f0b014395c84417f85f278daf41937
S.Giorgetta <Shinji_Ikari_1983@hotmail.com>
2018-12-11 17:21:48 +0100

    Navegación de Home a Página 1, funcional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,27 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+// import { Pagina1PageModule } from '../pages/pagina1/pagina1.module'; // <-- Añadido al agregar a apartado imports la Pagina1PageModule
+import { Pagina1Page } from './../pages/pagina1/pagina1'; // <-- Importado a través de la bombilla del IntelliSense tras añadir la clase Pagina1Page a los apartados entryComponents y declarations
+
+
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    Pagina1Page
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    // Pagina1PageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    Pagina1Page
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Pagina1Page } from '../pagina1/pagina1'; // <-- Autoimportada al añadir la clase al método irAPagina1()
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,5 +12,11 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+
+// Método para ir a la página pagina1
+irAPagina1():void{
+  this.navCtrl.push(Pagina1Page);
+}
 
 }
